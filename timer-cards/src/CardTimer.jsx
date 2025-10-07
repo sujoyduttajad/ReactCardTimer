f4ebb4c184ba6dcc01f36c06158d9b1a4334e10c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2025-10-07 16:27:12 -0600

    Update CardTimer.jsx

--- timer-cards/src/CardTimer.jsx
+++ timer-cards/src/CardTimer.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { formatMMSS } from "./utils/functions";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
@@ -20,11 +18,11 @@ const MemoizedCardTimer = React.memo(function CardTimer({
   return (
     <Box>
       <Card variant="outlined" sx={{ minWidth: 200, mb: 2, p: 2}}>
-        <Stack>
+        <Stack >
           <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
             {label}
           </Typography>
-          <Box>
+          <Box display={"flex"} alignItems={"center"} spacing={2} justifyContent={"space-between"} mb={1}>
             <Typography variant="h5" component="div">{formatMMSS(currentSeconds)}</Typography>
             {!isRunning ? (
               <Button onClick={() => onStart(id)} aria-label={`Start ${label}`}>
@@ -37,7 +35,7 @@ const MemoizedCardTimer = React.memo(function CardTimer({
             )}
           </Box>
 
-          <div>Random tick: {intervalMs} ms</div>
+          <Typography>Random tick: {intervalMs} ms</Typography>
         </Stack>
       </Card>
     </Box>
